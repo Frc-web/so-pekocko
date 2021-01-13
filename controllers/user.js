c55c84e590f31a87419968ce7237ea1c2d2f4acc
Freddy Caron <freddycaron.1@gmail.com>
2021-01-13 14:33:51 +0100

    masquage de l'adresse mail

--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,36 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+const MaskData = require('maskdata');
+const emailMask = {
+    maskWith: "*", 
+    unmaskedStartCharactersBeforeAt: 3,
+    unmaskedEndCharactersAfterAt: 3,
+    maskAtTheRate: false
+};
+
 const User = require("../models/User");
 const schemaPasswordValidator = require("../models/PasswordValidator");
 
 exports.signup = (req, res, next) => {
+    const mail = req.body.email;
+    const maskedEmail = MaskData.maskEmail2(mail, emailMask);
     if (schemaPasswordValidator.validate(req.body.password)) {
         bcrypt
-            .hash(req.body.password, 10)  /* 10 représente le salt, le nombre de tours que fait l'algorythme de hashage */
+            .hash(req.body.password, 10)  /* 10 représente le salt, le nombre de tours que fait l'algorythme de hashage */      
             .then(hash => {
                 const user = new User({
-                    email: req.body.email,
+                    email: maskedEmail,
                     password: hash,
                 });
+            // .then(hash => {
+            //     //masquage de l'adresse mail avant l'envoie dans la BD avec un chiffrement base 64
+            //     let buffer = new Buffer.from(req.body.email);
+            //     let hiddenMail = buffer.toString('base64');
+            //     const user = new User({
+            //       email: hiddenMail,
+            //       password: hash
+            //     });
                 user
                     .save()
                     .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
@@ -27,7 +45,13 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+    const mail = req.body.email;
+    const maskedEmail = MaskData.maskEmail2(mail, emailMask);
+    User.findOne({ email: maskedEmail })
+     //création d'un chiffrement base 64 sur l'adresse mail avant la vérification de l'adresse mail
+    // let buffer = new Buffer.from(req.body.email);
+    // let hiddenMail = buffer.toString('base64');
+    // User.findOne({ email: hiddenMail })
         .then(user => {
             if (!user) {
                 return res.status(401).json({ error: "Utilisateur non trouvé !" });
